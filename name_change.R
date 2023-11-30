b4d7086a0ce6cfadc112d3ab741399e5c996ecee
yhan <yushi.han@pasteur.fr>
2023-11-30 16:42:12 +0100

    backbone of the functions info()

--- name_change.R
+++ name_change.R
@@ -12,24 +12,25 @@
 #' @details 
 #' REQUIRED PACKAGES
 #' 
-#' none
+#' cuteDev
 #' 
 #' 
 #' REQUIRED FUNCTIONS FROM CUTE_LITTLE_R_FUNCTION
 #' 
-#' fun_check()
+#' arg_check()
 #' @examples
 #' obs1 <- c("A", "B", "C", "D") ; 
 #' obs2 <- c("A", "C") ; 
-#' fun_name_change(obs1, obs2)
+#' name_change(obs1, obs2)
 #' 
 #' 
 #' obs1 <- c("A", "B", "C", "C_modif1", "D") ; 
 #' obs2 <- c("A", "A_modif1", "C") ; 
-#' fun_name_change(obs1, obs2) 
+#' name_change(obs1, obs2) 
 #' # the function checks that the new names are neither in obs1 nor in obs2 (increment the number after the added string)
+#' @importFrom cuteDev name_change
 #' @export
-fun_name_change <- function(
+name_change <- function(
         data1, 
         data2, 
         added.string = "_modif"
@@ -38,28 +39,30 @@ fun_name_change <- function(
     # DEBUGGING
     # data1 = c("A", "B", "C", "D") ; data2 <- c("A", "C") ; added.string = "_modif" # for function debugging
     # function name
-    function.name <- paste0(as.list(match.call(expand.dots = FALSE))[[1]], "()")
+    ini <- match.call(expand.dots = FALSE) # initial parameters (specific of arg_test())
+    function.name <- paste0(as.list(match.call(expand.dots = FALSE))[[1]], "()") # function name with "()" paste, which split into a vector of three: c("::()", "package()", "function()") if "package::function()" is used.
+    if(function.name[1] == "::()"){
+        function.name <- function.name[3]
+    }
     arg.names <- names(formals(fun = sys.function(sys.parent(n = 2)))) # names of all the arguments
     arg.user.setting <- as.list(match.call(expand.dots = FALSE))[-1] # list of the argument settings (excluding default values not provided by the user)
     # end function name
-    # required function checking
-    req.function <- c(
-        "fun_check"
+    # package checking
+    # check of lib.path
+    # end check of lib.path
+
+   # check of the required function from the required packages
+    .pack_and_function_check <- function(
+        req.package = c(
+            "cuteDev::arg_check"
+        ),
+        lib.path = NULL,
+        external.function.name = function.name
     )
-    tempo <- NULL
-    for(i1 in req.function){
-        if(length(find(i1, mode = "function")) == 0L){
-            tempo <- c(tempo, i1)
-        }
-    }
-    if( ! is.null(tempo)){
-        tempo.cat <- paste0("ERROR IN ", function.name, "\nREQUIRED cute FUNCTION", ifelse(length(tempo) > 1, "S ARE", " IS"), " MISSING IN THE R ENVIRONMENT:\n", paste0(tempo, collapse = "()\n"))
-        stop(paste0("\n\n================\n\n", tempo.cat, "\n\n================\n\n"), call. = FALSE) # == in stop() to be able to add several messages between ==
-    }
-    # end required function checking
-    # reserved words (to avoid bugs)
-    # end reserved words (to avoid bugs)
-    
+    # end check of the required function from the required packages
+    # end package checking
+
+    # argument primary checking
     # arg with no default values
     mandat.args <- c(
         "data1", 
@@ -72,20 +75,23 @@ fun_name_change <- function(
     }
     # end arg with no default values
     
-    # argument primary checking
-    arg.check <- NULL #
+    # argument checking with arg_check()
+    argum.check <- NULL #
     text.check <- NULL #
     checked.arg.names <- NULL # for function debbuging: used by r_debugging_tools
-    ee <- expression(arg.check <- c(arg.check, tempo$problem) , text.check <- c(text.check, tempo$text) , checked.arg.names <- c(checked.arg.names, tempo$object.name))
-    tempo <- fun_check(data = data1, class = "vector", mode = "character", fun.name = function.name) ; eval(ee)
-    tempo <- fun_check(data = data2, class = "vector", mode = "character", fun.name = function.name) ; eval(ee)
-    tempo <- fun_check(data = added.string, class = "vector", mode = "character", length = 1, fun.name = function.name) ; eval(ee)
-    if( ! is.null(arg.check)){
-        if(any(arg.check, na.rm = TRUE) == TRUE){
-            stop(paste0("\n\n================\n\n", paste(text.check[arg.check], collapse = "\n"), "\n\n================\n\n"), call. = FALSE) #
+    ee <- expression(argum.check <- c(argum.check, tempo$problem) , text.check <- c(text.check, tempo$text) , checked.arg.names <- c(checked.arg.names, tempo$object.name))
+    tempo <- cuteDev::arg_check(data = data1, class = "vector", mode = "character", fun.name = function.name) ; eval(ee)
+    tempo <- cuteDev::arg_check(data = data2, class = "vector", mode = "character", fun.name = function.name) ; eval(ee)
+    tempo <- cuteDev::arg_check(data = added.string, class = "vector", mode = "character", length = 1, fun.name = function.name) ; eval(ee)
+    if( ! is.null(argum.check)){
+        if(any(argum.check, na.rm = TRUE) == TRUE){
+            stop(paste0("\n\n================\n\n", paste(text.check[argum.check], collapse = "\n"), "\n\n================\n\n"), call. = FALSE) #
         }
     }
-    # source("C:/Users/Gael/Documents/Git_versions_to_use/debugging_tools_for_r_dev-v1.7/r_debugging_tools-v1.7.R") ; eval(parse(text = str_basic_arg_check_dev)) ; eval(parse(text = str_arg_check_with_fun_check_dev)) # activate this line and use the function (with no arguments left as NULL) to check arguments status and if they have been checked using fun_check()
+    # end argument checking with arg_check()
+    # check with r_debugging_tools
+    # source("C:/Users/yhan/Documents/Git_projects/debugging_tools_for_r_dev/r_debugging_tools.R") ; eval(parse(text = str_basic_arg_check_dev)) ; eval(parse(text = str_arg_check_with_fun_check_dev)) # activate this line and use the function (with no arguments left as NULL) to check arguments status and if they have been checked using arg_check()
+    # end check with r_debugging_tools
     # end argument primary checking
     
     # second round of checking and data preparation
@@ -115,16 +121,14 @@ fun_name_change <- function(
     
     # code that protects set.seed() in the global environment
     # end code that protects set.seed() in the global environment
-    
     # warning initiation
     # end warning initiation
-    
     # other checkings
     # end other checkings
-    
-    # reserved word checking
-    # end reserved word checking
+    # reserved words (to avoid bugs)
+    # end reserved words (to avoid bugs)
     # end second round of checking and data preparation
+
     
     # package checking
     # end package checking
@@ -154,6 +158,8 @@ fun_name_change <- function(
         data <- data1
     }
     # output
+    # warning output
+    # end warning output
     output <- list(data = data, ini = ini, post = post)
     return(output)
     # end output
