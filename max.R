d2f31003768253807c243f7dc81ee5a620e5c565
yhan <yushi.han@pasteur.fr>
2023-12-06 11:50:14 +0100

    changement files min() and max()

--- max.R
+++ max.R
@@ -1,10 +1,10 @@
 #' @title max
 #' @description
-#' Find and return the minimum value of a set of data.
+#' Find and return the maximum value of a set of data.
 #' @param x Numeric or logical vector or matrix or numeric table where find the initial values to use.
 #' @param na.rm Single logical value. Should missing values (NA and NaN) be removed ?
 #' @param finite Single logical value. Should infinite values (Inf and -Inf) be removed ? Warning: this argument does not remove NA and NaN. Please use the na.rm argument.
-#' @returns The minimum value and the maximum value of the given arguments. 
+#' @returns The maximum value of the given arguments. 
 #' @details
 #' REQUIRED PACKAGES
 #' 
@@ -15,13 +15,13 @@
 #' 
 #' arg_check()
 #' @examples
-#' vec <- c(1:3) ; range(x = vec)
+#' vec <- c(1:3) ; max(x = vec)
 #' 
-#' vec <- c(1,3,5,TRUE) ; range(x = vec)
+#' vec <- c(1,3,5,TRUE) ; max(x = vec)
 #' 
 #' 
 #' # This example returns an error because of the character in the vector
-#' # vec <- c(1,3,5,TRUE,"apple") ; range(x = vec)
+#' # vec <- c(1,3,5,TRUE,"apple") ; max(x = vec)
 #' @importFrom cuteDev arg_check
 #' @export
 max <- function(
--- min.R
+++ min.R
@@ -4,7 +4,7 @@
 #' @param x Numeric or logical vector or matrix or numeric table where find the initial values to use.
 #' @param na.rm Single logical value. Should missing values (NA and NaN) be removed ?
 #' @param finite Single logical value. Should infinite values (Inf and -Inf) be removed ? Warning: this argument does not remove NA and NaN. Please use the na.rm argument.
-#' @returns The minimum value and the maximum value of the given arguments. 
+#' @returns The minimum value of the given arguments. 
 #' @details
 #' REQUIRED PACKAGES
 #' 
@@ -15,13 +15,13 @@
 #' 
 #' arg_check()
 #' @examples
-#' vec <- c(1:3) ; range(x = vec)
+#' vec <- c(1:3) ; min(x = vec)
 #' 
-#' vec <- c(1,3,5,TRUE) ; range(x = vec)
+#' vec <- c(1,3,5,TRUE) ; min(x = vec)
 #' 
 #' 
 #' # This example returns an error because of the character in the vector
-#' # vec <- c(1,3,5,TRUE,"apple") ; range(x = vec)
+#' # vec <- c(1,3,5,TRUE,"apple") ; min(x = vec)
 #' @importFrom cuteDev arg_check
 #' @export
 min <- function(
