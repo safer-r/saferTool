901ef7d1d580873a5992ed611d36e66168989198
yhan <yushi.han@pasteur.fr>
2024-02-05 16:27:50 +0100

    add test comp_2d() and comp_list()

--- tests/testthat/test-comp_2d.R
+++ tests/testthat/test-comp_2d.R
@@ -1,3 +1,46 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("comp_2d function works correctly", {
+  # Example datasets
+  mat1 <- matrix(1:1e6, ncol = 5, dimnames = list(NULL, LETTERS[1:5]))
+  mat2 <- matrix(as.integer((1:1e6) + 1e6/5), ncol = 5, dimnames = list(NULL, LETTERS[1:5]))
+  mat3 <- matrix((1:1e6) + 1e6/5, ncol = 5, dimnames = list(NULL, LETTERS[1:5]))
+  mat4 <- matrix(1:10, byrow = TRUE, ncol = 5, dimnames = list(letters[1:2], LETTERS[1:5]))
+  mat5 <- matrix(c(1:5, 101:105, 6:10), byrow = TRUE, ncol = 5, dimnames = list(c("a", "z", "b"), c(LETTERS[1:2], "k", LETTERS[5:4])))
+  mat6 <- matrix(c(1:5, 101:105, 6:10), byrow = TRUE, ncol = 5, dimnames = list(c("x", "z", "y"), c(LETTERS[1:2], "k", LETTERS[5:4])))
+  dataframe1 <- as.data.frame(matrix(1:10, byrow = TRUE, ncol = 5, dimnames = list(letters[1:2], LETTERS[1:5])))
+  dataframe2 <- as.data.frame(matrix(c(1:5, 101:105, 6:10), byrow = TRUE, ncol = 5, dimnames = list(c("a", "z", "b"), c(LETTERS[1:2], "k", LETTERS[5:4]))))
+  dataframe3 <- as.data.frame(matrix(c(1:5, 101:105, 6:10), byrow = TRUE, ncol = 5, dimnames = list(c("x", "z", "y"), c(LETTERS[1:2], "k", LETTERS[5:4]))))
+
+  # Test large matrices with the same content
+  test_that("handles large matrices with the same content", {
+    expect_no_error(comp_2d(mat1, mat2))
+  })
+
+  # Test large matrices with different content
+  test_that("handles large matrices with different content", {
+    expect_no_error(comp_2d(mat1, mat3))
+  })
+
+  # Test matrices with the same row content and same row names
+  test_that("handles matrices with the same row content and same row names", {
+    expect_no_error(comp_2d(mat4, mat5))
+  })
+
+  # Test matrices with the same row content but different row names
+  test_that("handles matrices with the same row content but different row names", {
+    expect_no_error(comp_2d(mat4, mat6))
+  })
+
+  # Test data frames with the same row content and same row names, not same mode between columns
+  test_that("handles data frames with the same row content and same row names, not same mode between columns", {
+    dataframe1[, 5] <- as.character(dataframe1[, 5])
+    dataframe2[, 5] <- as.character(dataframe2[, 5])
+    expect_no_error(comp_2d(dataframe1, dataframe2))
+  })
+
+  # Test data frames with the same row content but different row names
+  test_that("handles data frames with the same row content but different row names", {
+    dataframe1[, 5] <- as.character(dataframe1[, 5])
+    dataframe3[, 5] <- as.character(dataframe3[, 5])
+    expect_no_error(comp_2d(dataframe1, dataframe3))
+  })
 })
--- tests/testthat/test-comp_list.R
+++ tests/testthat/test-comp_list.R
@@ -1,3 +1,23 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("comp_list function works correctly", {
+  # Example datasets
+  list1 <- list(a = 1:5, b = LETTERS[1:2], d = matrix(1:6)) # 2D list
+  list2 <- list(1:5, LETTERS[1:2]) # 2D list
+  list3 <- list(a = 1:5, b = LETTERS[1:2]) # 2D list
+  list4 <- list(LETTERS[5:9], matrix(1:6), 1:5) # 2D list
+
+  # Test cases
+  # Test lists with the same content
+  test_that("handles lists with the same content", {
+    expect_no_error(comp_list(list1, list1))
+  })
+
+  # Test lists with different content
+  test_that("handles lists with different content", {
+    expect_no_error(comp_list(list1, list3))
+  })
+
+  # Test lists with different lengths
+  test_that("handles lists with different lengths", {
+    expect_no_error(comp_list(list2, list4))
+  })
 })
