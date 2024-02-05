f94483390e15910538f2e635a4ff1778b72da549
yhan <yushi.han@pasteur.fr>
2024-02-05 16:46:26 +0100

    add tests info() and max2()

--- tests/testthat/test-info.R
+++ tests/testthat/test-info.R
@@ -1,3 +1,54 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("info function works correctly", {
+  # Example datasets
+  vec1 <- -1:3
+  vec2 <- 1:3 / 3
+  vec3 <- c(1, 2, NA, -Inf)
+  vec4 <- "pearson"
+  vec5 <- c("a", "b", "a", NA)
+  mat1 <- matrix(vec1)
+  dimnames(mat1) <- list(NULL, "M1")
+  mat2 <- matrix(c(1:5, NA), ncol = 2, dimnames = list(c("ROW1", "ROW2", "ROW3"), c("M1", "M2")))
+  df1 <- as.data.frame(mat2)
+  l1 <- list(L1 = 1:3, L2 = letters[1:3])
+  fac1 <- factor(rep(letters[4:6], c(4:6)))
+  tab1 <- table(fac1)
+  tab2 <- table(fac1, fac1)
+  exp1 <- expression("a")
+  name1 <- substitute(exp1)
+  fun1 <- mean
+  fun2 <- sum
+  fun3 <- get("<-")
+  env1 <- new.env()
+  s4 <- show
+  call1 <- call("call1")
+
+  # Test cases
+  # Simple examples
+  test_that("handles simple examples", {
+    expect_no_error(info(data = vec1))
+    expect_no_error(info(data = vec2))
+    expect_no_error(info(data = vec3))
+    expect_no_error(info(data = vec4))
+    expect_no_error(info(data = vec5))
+    expect_no_error(info(data = mat1))
+    expect_no_error(info(data = mat2))
+    expect_no_error(info(data = df1))
+    expect_no_error(info(data = l1))
+    expect_no_error(info(data = fac1))
+    expect_no_error(info(data = tab1))
+    expect_no_error(info(data = tab2))
+    expect_no_error(info(data = exp1))
+    expect_no_error(info(data = name1))
+    expect_no_error(info(data = fun1))
+    expect_no_error(info(data = fun2))
+    expect_no_error(info(data = fun3))
+    expect_no_error(info(data = env1))
+    expect_no_error(info(data = s4))
+    expect_no_error(info(data = call1))
+  })
+
+  # Test all arguments
+  test_that("handles all arguments", {
+    expect_no_error(info(data = vec1, n = 1, warn.print = FALSE))
+  })
 })
--- tests/testthat/test-max2.R
+++ tests/testthat/test-max2.R
@@ -1,3 +1,84 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("max2 function works correctly", {
+  # Example datasets
+  vec1 <- -1:3
+  vec2 <- c(1:3, NA)
+  vec3 <- c(1:3, Inf)
+  vec4 <- 1:3 / 3
+  vec5 <- c(1, 2, NA, -Inf)
+  log1 <- c(TRUE, FALSE, NA)
+  log2 <- c(TRUE, FALSE, FALSE)
+  mat1 <- matrix(vec1)
+  mat2 <- matrix(vec5)
+  mat3 <- matrix(log1)
+  mat4 <- matrix(log2)
+  fac1 <- factor(vec5)
+  tab1 <- table(fac1)
+
+  # Test cases
+  test_that("handles vector of integers", {
+    expect_equal(max2(x = vec1), 3)
+  })
+
+  test_that("handles vector of integers with NA", {
+    expect_equal(max2(x = vec2), 3)
+  })
+
+  test_that("handles vector of integers with Inf", {
+    expect_equal(max2(x = vec3), Inf)
+  })
+
+  test_that("handles vector of proportions", {
+    expect_equal(max2(x = vec4), 1)
+  })
+
+  test_that("handles vector of integers but stored as double, with NA and Inf", {
+    expect_equal(max2(x = vec5), Inf)
+  })
+
+  test_that("handles logical vector with NA", {
+    expect_error(max2(x = log1), "max2 does not support logical vectors.")
+  })
+
+  test_that("handles logical vector", {
+    expect_error(max2(x = log2), "max2 does not support logical vectors.")
+  })
+
+  test_that("handles 1D matrix of integers", {
+    expect_equal(max2(x = mat1), 3)
+  })
+
+  test_that("handles 1D matrix of integers, NA, and Inf", {
+    expect_equal(max2(x = mat2), Inf)
+  })
+
+  test_that("handles 1D matrix of logical values", {
+    expect_no_error(max2(x = mat3))
+  })
+
+  test_that("handles 1D matrix of logical values and NA", {
+    expect_no_error(max2(x = mat4))
+  })
+
+  test_that("handles 1D table", {
+    expect_no_error(max2(x = tab1))
+  })
+
+  # Test arguments
+  test_that("handles na.rm argument", {
+    expect_equal(max2(x = vec1, na.rm = TRUE), 3)
+    expect_equal(max2(x = vec2, na.rm = TRUE), 3)
+    expect_no_error(max2(x = log2, na.rm = TRUE))
+    expect_no_error(max2(x = log1, na.rm = TRUE))
+    expect_no_error(max2(x = mat3, na.rm = TRUE))
+  })
+
+  test_that("handles finite argument", {
+    expect_equal(max2(x = vec1, finite = TRUE), 3)
+    expect_no_error(max2(x = vec3, finite = TRUE))
+  })
+
+  test_that("handles all arguments", {
+    expect_no_error(max2(x = vec5, na.rm = TRUE, finite = FALSE))
+    expect_no_error(max2(x = mat4, na.rm = TRUE, finite = FALSE))
+  })
 })
