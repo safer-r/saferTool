51b15702aae7258444e0d98c1bf4265fd8580bd7
yhan <yushi.han@pasteur.fr>
2024-02-06 15:13:58 +0100

    add tests for internal function

---
+++ tests/testthat/test-internal_function.R
@@ -0,0 +1,43 @@
+test_that(".pack_and_function_check handles valid input correctly", {
+
+  # Mock inputs
+  fun <- "geom_point"  # incorrect input
+  
+  # Call the function and expect an error
+  expect_error(.pack_and_function_check(fun, "path/to/library", "fun1"))
+
+  # Mock inputs
+  fun <- "ggplot2::geom_point"
+  lib.path <- "path/to/nonexistent/library"  # incorrect library path
+  
+  # Call the function and expect an error
+  expect_error(.pack_and_function_check(fun, lib.path, "fun1"))
+
+  # Mock inputs
+  fun <- "ggplot2::non_existent_function"  # incorrect function name
+  
+  # Call the function and expect an error
+  expect_error(.pack_and_function_check(fun, "path/to/library", "fun1"), 
+               regexp = "ERROR IN fun1.*REQUIRED FUNCTION.*MISSING IN THE INSTALLED PACKAGE")
+
+
+  # Mock inputs
+  x <- "not_numeric_or_logical"
+  na.rm <- TRUE
+  
+  # Call the function and expect an error
+  expect_error(.arguments_check(x, na.rm, FALSE, "test_function"))
+
+  # Mock inputs
+  x <- 1:10
+  na.rm <- "not_logical"
+  
+  # Call the function and expect an error
+  expect_error(.arguments_check(x, na.rm, FALSE, "test_function"))
+
+  # Mock inputs
+  na.rm <- TRUE
+  
+  # Call the function and expect an error
+  expect_error(.arguments_check(NULL, na.rm, FALSE, "test_function"))
+})
