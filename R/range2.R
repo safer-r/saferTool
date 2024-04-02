f2a160866beab9250d45a489ee5603cd4e19fa4a
hwang <haiding.wang@pasteur.fr>
2024-04-02 12:36:48 +0200

    completed pacakge names

--- R/range2.R
+++ R/range2.R
@@ -26,7 +26,7 @@ range2 <- function(
     package.name <- "saferTool"
     # end package name
     # function name
-    function.name <- paste0(as.list(match.call(expand.dots = FALSE))[[1]], "()") # function name with "()" paste, which split into a vector of three: c("::()", "package()", "function()") if "package::function()" is used.
+    function.name <- base::paste0(base::as.list(base::match.call(expand.dots = FALSE))[[1]], "()") # function name with "()" paste, which split into a vector of three: c("::()", "package()", "function()") if "package::function()" is used.
     if(function.name[1] == "::()"){
         function.name <- function.name[3]
     }
@@ -43,6 +43,6 @@ range2 <- function(
     # output
     # warning output
     # end warning output
-    return(output)
+    base::return(output)
     # end output
 }
