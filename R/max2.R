285919a5ea0d3ee3917391213013c8f1136d1c49
hwang <haiding.wang@pasteur.fr>
2024-04-02 10:36:34 +0200

    completed package names

--- R/max2.R
+++ R/max2.R
@@ -29,7 +29,7 @@ max2 <- function(
     package.name <- "saferTool"
     # end package name
     # function name
-    function.name <- paste0(as.list(match.call(expand.dots = FALSE))[[1]], "()") # function name with "()" paste, which split into a vector of three: c("::()", "package()", "function()") if "package::function()" is used.
+    function.name <- base::paste0(base::as.list(base::match.call(expand.dots = FALSE))[[1]], "()") # function name with "()" paste, which split into a vector of three: c("::()", "package()", "function()") if "package::function()" is used.
     if(function.name[1] == "::()"){
         function.name <- function.name[3]
     }
@@ -42,13 +42,13 @@ max2 <- function(
     )
     # main code
     if(finite == TRUE){
-        x <- x[ ! x %in% c(Inf, -Inf)]
+        x <- x[ ! x %in% base::c(Inf, -Inf)]
     }
     output <- base::max(x, na.rm = na.rm)
     # end main code
     # output
     # warning output
     # end warning output
-    return(output)
+    base::return(output)
     # end output
 }
