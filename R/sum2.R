dcce36b965096dcafec0bf7d43c98c90ac585f6a
hwang <haiding.wang@pasteur.fr>
2024-04-09 14:55:07 +0200

    ADD THE SAFER_CHECK ARGUMENT

--- R/sum2.R
+++ R/sum2.R
@@ -4,6 +4,7 @@
 #' @param x Numeric or logical vector or matrix or numeric table where find the initial values to calculate.
 #' @param na.rm Single logical value. Should missing values (NA and NaN) be removed ?
 #' @param finite Single logical value. Should infinite values (Inf and -Inf) be removed ? Warning: this argument does not remove NA and NaN. Please use the na.rm argument.
+#' @param safer_check Single logical value. Perform some "safer" checks (see https://github.com/safer-r)? If TRUE, checkings are performed before main code running: 1) R classical operators (like "<-") not overwritten by another package because of the R scope and 2) required functions and related packages effectively present in local R lybraries. Set to FALSE if this fonction is used inside another "safer" function to avoid pointless multiple checkings.
 #' @returns The sum of the given arguments. Returns NA if the input contains NA and the argument na.rm = TRUE, else returns a value.
 #' @examples
 #' vec <- c(1:3) ; sum2(x = vec)
@@ -18,7 +19,8 @@
 sum2 <- function(
         x,
         na.rm = FALSE,
-        finite = FALSE
+        finite = FALSE,
+        safer_check = TRUE
 ){
     # DEBUGGING
     # vec <- c(1,3,5,TRUE) ; sum2(x = vec) # for function debugging
