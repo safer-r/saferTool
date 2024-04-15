bd3486e7c722903d59c53b7b5bf53e8264a56140
hwang <haiding.wang@pasteur.fr>
2024-04-15 15:27:12 +0200

    add external.package.name = package.name

--- R/round2.R
+++ R/round2.R
@@ -43,6 +43,16 @@ round2 <- function(
     arg.names <- base::names(base::formals(fun = base::sys.function(base::sys.parent(n = 2)))) # names of all the arguments
     arg.user.setting <- base::as.list(base::match.call(expand.dots = FALSE))[-1] # list of the argument settings (excluding default values not provided by the user)
     # end function name
+    # critical operator checking
+    if(safer_check == TRUE){
+        .base_op_check(
+            external.function.name = function.name,
+            external.package.name = package.name
+    )
+    }
+    # end critical operator checking
+
+
     # package checking
     # check of lib.path
     # end check of lib.path
